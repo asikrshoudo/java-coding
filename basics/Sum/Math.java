23daccb94d4e42b35d8851b4b299886d0502565f
asikrshoudo <shoudoashikurshek@gmail.com>
2026-02-13 13:31:24 +0600

    added my first project

---
+++ basics/Sum/Math.java
@@ -0,0 +1,14 @@
+public class Math {
+
+    public static void main(String[] args) {
+        short firstShortNumber;
+        int secondShortNumber;
+
+        firstShortNumber = 10;
+        secondShortNumber = 22;
+
+        int result = firstShortNumber + secondShortNumber;
+
+        System.out.println(result);
+    }
+}
