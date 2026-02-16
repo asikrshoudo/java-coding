5081d6634407e0e1f96116428435bc61432b5b71
asikrshoudo <shoudoashikurshek@gmail.com>
2026-02-16 19:35:48 +0600

    hmmm

---
+++ Test-2/main.java
@@ -0,0 +1,20 @@
+public class main{
+  public static void main(String[]args){
+    
+    // This is java coding. 
+    /*
+    lets try multiline comments
+    */
+    int age = 17;
+    Boolean isAge = true;
+    
+    if(isAge){
+      System.out.println("Yes you can access here because your age is " + age);
+    }
+    else{
+      System.out.println("sorry you can't");
+    }
+    
+    
+  }
+}
