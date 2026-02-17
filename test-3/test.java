16c8308c4a125b68a03eee3f3aaf639600946999
asikrshoudo <shoudoashikurshek@gmail.com>
2026-02-17 23:57:47 +0600

    hmmm

--- test-3/test.java
+++ test-3/test.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
-
 public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("what is your name: ");
         String name = sc.nextLine();
-
         if (name.equals("shoudo")) {
             System.out.println("hello, " + name + " how are you?");
         } else {
             System.out.println("sorry who are you?");
         }
-
         sc.close();
     }
 }
